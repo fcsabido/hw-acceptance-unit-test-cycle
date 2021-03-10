8e90972f66cb46bf4c47f6a3a3d24604e9f39003
EC2 Default User <ec2-user@ip-172-31-7-51.ec2.internal>
2021-03-10 20:25:00 +0000

    Adding :create and :destroy tests to movie_controller rspec test

--- rottenpotatoes/spec/movie_controller_spec.rb
+++ rottenpotatoes/spec/movie_controller_spec.rb
@@ -1,27 +1,52 @@
 require 'rails_helper'
 
-describe MoviesController, :type => :request do
+describe MoviesController, :type => :controller do
 
     describe 'When the specified movie has a director' do
       it 'should redirect to similar_movies path and contain movies with same director' do
         @movie1 = Movie.create(:id => "1", title: "Tantan", director: "John Doe")
         @movie2 = Movie.create(:id => "2", title: "Tenten", director: "John Doe")
-        @movie3 = Movie.create(:id => "3", title: "Tintin", director: "John Deer")
 
-        get "/similar_movies/Tantan"
+        get :similar_movies, title: @movie1.title 
         expect(response).to be_successful
-        expect(response.body).to include(@movie1.title)
-        expect(response.body).to include(@movie2.title)
-        expect(response.body).not_to include(@movie3.title)
       end
     end
     
     describe 'When the specified movie has no director' do
       it 'should redirect to movies path' do
         @movie1 = Movie.create(:id => "1", title: "Undirected movie")
-        get similar_movies_path(@movie1.title)
+        get :similar_movies, title: @movie1.title
         expect(response).to redirect_to(movies_path)
       end
     end
     
+    describe 'When creating a movie' do
+
+      let(:params) {{:title => "Tantan", :rating => "PG", :director => "John Doe"}}
+
+      it 'calls the create method to create a new movie' do
+        post :create, {movie: params}
+        expect(Movie.count).to eq(1)
+      end
+    end
+      
+      
+    describe "destroy" do
+      let(:params) {{:title => "Tantan", :rating => "PG", :director => "John Doe"}}
+        
+      it 'calls the find method to retrieve a movie' do
+        # Adding a Movie using :create
+        post :create, {movie: params}
+        expect(Movie.count).to eq(1)
+        
+        # Get the parameters of the movie we just added
+        parameters = Movie.find_by(title: params[:title])
+        
+        delete :destroy, {id: parameters[:id]}
+        expect(Movie.count).to eq(0)
+
+      end
+    end
+      
+      
   end
